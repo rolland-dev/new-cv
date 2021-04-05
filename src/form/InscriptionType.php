9d9a4371200fb5619f61dd02188670152bb475a5
Didier <didier@rolland-dev.fr>
2021-04-05 10:34:40 +0200

    modif

--- src/form/InscriptionType.php
+++ src/form/InscriptionType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class InscriptionType extends AbstractType
@@ -13,10 +15,26 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('password', PasswordType::class)
-            ->add('confirm_password',PasswordType::class)
+            ->add('email', EmailType::class ,[
+                'attr'=>[
+                    'placeholder'=> '@mail',
+                ]
+            ])
+            ->add('username', TextType::class ,[
+                'attr'=>[
+                    'placeholder'=> 'Nom utilisateur',
+                ]
+            ])
+            ->add('password', PasswordType::class ,[
+                'attr'=>[
+                    'placeholder'=> 'Mot de passe',
+                ]
+            ])
+            ->add('confirm_password',PasswordType::class ,[
+                'attr'=>[
+                    'placeholder'=> 'Confirmez mot de passe',
+                ]
+            ])
         ;
     }
 
